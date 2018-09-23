10096df114c190eee75fa25b3a8da9ffb0b27456
DavidWei <david.wei@schleuniger.com>
2018-09-23 20:47:47 +0800

    layout with PhotonKit

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
-const {app, BrowserWindow} = require('electron')
+const {app, Menu, BrowserWindow} = require('electron')
 
-let win = null;
+let win;
 
 function createWindow () {
 
-  win = new BrowserWindow({width: 800, height: 600})
-
-  win.loadFile('file://${__dirname}/html/index.html')
+  win = new BrowserWindow({width: 800, height: 600, show: false, fullscreenable: false, resizable: false, maximizable: false, })
+  
+  win.loadFile('html/index.html')
 
   // 打开开发者工具
   //win.webContents.openDevTools()
 
   win.on('closed', () => {
-    console.log("main window closed")
     win = null
+  })
+  win.setMenu(null);
+  win.once('ready-to-show', () => {
+    win.show()
   })
 }
 
 app.commandLine.appendSwitch('remote-debugging-port', '8315')
 app.commandLine.appendSwitch('host-rules', 'MAP * 127.0.0.1')
 app.on('ready', createWindow);
-
 
 // app.on('window-all-closed', () => {
 //   if (process.platform !== 'darwin') {
