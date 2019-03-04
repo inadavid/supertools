393c659deaaf2747338843d33efe54bd1b3fa1cf
Wei Liang (David) <David.Wei@schleuniger.com>
2019-03-04 22:49:58 +0800

    pre release V0171;bom upload history (for temp usage);

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,91 +1,42 @@
 $(() => {
-    var sql_history = sqlite.run("select sn, remark, bom_top, stat, time from bom order by sn desc;");
-    var tbody = '<tr><td colspan="6"> 还没有SQL生成记录 </td></tr>';
-    if (sql_history.length != 0) {
-        tbody = "";
-        for (var i = 0; i < sql_history.length; i++) {
-            tbody += "<tr>";
-            tbody += "<td>" + sql_history[i].sn + "</td>";
-            tbody += "<td>" + sql_history[i].remark + "</td>";
-            tbody += "<td class='selectable'>" + sql_history[i].bom_top + "</td>";
-            tbody += "<td>" + sql_history[i].time + "</td>";
-            tbody += "<td>" + (sql_history[i].stat == 0 ? "未执行" : (sql_history[i].stat == 1 ? "已执行" : "已删除")) + "</td>";
-            if (sql_history[i].stat == 0) tbody += "<td><button type='button' bid='exe' class='btn btn-success btn-xs custom' sn='" + sql_history[i].sn + "'>执行SQL，导入ERP系统</button> </td> ";
-            else if (sql_history[i].stat == 1) tbody += "<td><button type='button' bid='del' class='btn btn-danger btn-xs custom' sn='" + sql_history[i].sn + "'>回滚SQL，删除已经导入的内容</button> </td> ";
-            else tbody += "<td> </td> ";
-            tbody += "</tr>";
-        }
-    }
-    $("table[bid=dtable] tbody").append(tbody);
-
-    $("button[bid]").on("click", (e) => {
-        if (config.fSQLserver != 4) {
-            popup("数据库未准备好", "danger");
+    var sqltxt = "select a.*,b.opname from st_goodsbom_stat as a inner join m_operator as b on a.opid=b.opid where a.opid=" + user.id + " order by date desc;";
+    new sql.Request().query(sqltxt, (err, result) => {
+        if (err) {
+            alert("Error happened: \n" + JSON.stringify(err));
+            console.error(err)
             return;
         }
-        var sn = $(e.currentTarget).attr("sn");
-        var action = $(e.currentTarget).attr("bid");
-        var data = sqlite.run("select * from bom where sn =" + sn + " limit 1;")[0];
-        if (data == undefined) {
-            popup("数据读取出错！", "danger");
-            return;
-        }
-        if (action == "exe") {
-            if (!confirm("确定要执行导入操作么？本操作会导入" + data.rows + "条新的BOM数据。")) return;
-            (async () => {
-                try {
-                    await sql.query(data.sql_delete);
-                    await sql.query(data.sql_insert);
-                    sqlite.update("bom", {
-                        stat: 1
-                    }, {
-                        sn: sn
-                    });
-                    var moment = require('moment');
-                    var d = {
-                        time: moment().format("YYYY-MM-DD HH:mm:ss"),
-                        type: 1,
-                        data: JSON.stringify(data),
-                        remark: "insert bom items ok and done"
-                    };
-                    sqlite.insert("log", d);
-                    popup("\nBOM相关层级已经插入", "success");
-                    loadPanel("dashboard");
-                    loglog("ExecuteSQL-execution", data.sql_delete + " | " + data.sql_insert);
+        $('table[bid="dtable"] tbody').html("")
 
-                } catch (err) {
-                    // ... error checks
-                    popup(err + "\n请重新加载后再试。", "danger");
-                }
-            })()
+        if (result.rowsAffected == 0) {
+            $('table[bid="dtable"] tbody').append('<td colspan="6">No BOM in system.</td>');
+        } else {
+            for (var i in result.recordset) {
+                var data = result.recordset[i];
+                var tr = $("<tr>").attr("sn", data.sn).attr("top", data.bomtop);
+                tr.append($("<td>").text(data.sn));
+                tr.append($("<td>").text(data.bomtop));
+                tr.append($("<td>").text(codesInfo[data.bomtop].name + " | " + codesInfo[data.bomtop].spec));
+                tr.append($("<td>").text(data.opname));
+                tr.append($("<td>").text(moment(data.date).utc().format("YYYY-MM-DD HH:mm:ss")));
+                tr.append($("<td>").append($("<span class='iconfont icon-shanchu' bid='delete' sn=" + data.sn + ">")));
+                $('table[bid="dtable"] tbody').append(tr);
+            }
         }
-        if (action == "del") {
-            if (!confirm("确定要执行导入操作么？本操作会删除" + data.rows + "条的BOM数据。\n请注意，此操作不可逆！！！")) return;
-            (async () => {
-                try {
-                    await sql.query(data.sql_delete);
-                    sqlite.update("bom", {
-                        stat: 2
-                    }, {
-                        sn: sn
-                    });
-                    var moment = require('moment');
-                    var d = {
-                        time: moment().format("YYYY-MM-DD HH:mm:ss"),
-                        type: 2,
-                        data: JSON.stringify(data),
-                        remark: "delete bom items ok and done"
-                    };
-                    sqlite.insert("log", d);
-                    popup("\nBOM相关层级已经删除", "success");
-                    loadPanel("dashboard");
-                    loglog("ExecuteSQL-deletion", data.sql_delete);
 
-                } catch (err) {
-                    // ... error checks
-                    popup(err + "\n请重新加载后再试。", "danger");
+        $('table[bid="dtable"] tbody span.iconfont').css("cursor", "pointer").click(function () {
+            var code = $(this).parents("tr").attr("top").trim();
+            var sn = $(this).parents("tr").attr("sn").trim();
+            if (!confirm("Do you really want to detail bom of " + code + "?")) return false;
+            sqltxt = "delete from st_goodsbom where mark = " + sn + "; delete from st_bomtop where goodsid = '" + code + "'; delete from st_picklists where code = '" + code + "'; delete from st_goodsbom_stat where sn = " + sn + ";";
+            new sql.Request().query(sqltxt, (err, result) => {
+                if (err) {
+                    alert("Error happened: \n" + JSON.stringify(err));
+                    console.error(err)
+                    return;
                 }
-            })()
-        }
-    })
+                loadPanel("dashboard");
+            });
+        })
+    });
 })
