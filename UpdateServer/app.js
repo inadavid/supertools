23a40f1e268a9ea3cb02c2d788ad38cea1b3a37d
DavidWei <david.wei@schleuniger.com>
2019-03-05 08:41:51 +0800

    v0171 release

--- UpdateServer/app.js
+++ UpdateServer/app.js
@@ -10,23 +10,23 @@ app.use(bodyParser.urlencoded({
 
 var filepath = '/home/update' // or ../update.zip
 var fileurl = 'http://192.168.16.12:8082/update.7z';
-var vers=[];
+var vers = [];
 if (process.argv[2] == "dev") {
 	filepath = path.normalize(__dirname + "/../release-builds/update.7z") // or ../update.zip
 	fileurl = 'http://127.0.0.1:8082/update.7z';
 }
 const fs = require("fs");
-app.post('/update',function(req,res) {
+app.post('/update', function (req, res) {
 	var dir = fs.readdirSync(filepath)
 	vers = [];
-	for(var i in dir){
-		if(dir[i].indexOf('update.')!=-1){
-			var tv = dir[i].substr(dir[i].indexOf('update.')+7,dir[i].indexOf('.7z')-dir[i].indexOf('update.')-7);
-			if(tv.substr(0,1)=='0') vers.push(tv);
+	for (var i in dir) {
+		if (dir[i].indexOf('update.') != -1) {
+			var tv = dir[i].substr(dir[i].indexOf('update.') + 7, dir[i].indexOf('.7z') - dir[i].indexOf('update.') - 7);
+			if (tv.substr(0, 1) == '0') vers.push(tv);
 		}
 	}
 	res.write(JSON.stringify({
-		"last": "V"+vers[vers.length-1],
+		"last": "V" + vers[vers.length - 1],
 		"file": fileurl
 	}).replace(/[\/]/g, '\\/'));
 	console.log("reply to post @" + moment().format("HH:mm:ss"))
@@ -34,7 +34,7 @@ app.post('/update',function(req,res) {
 })
 
 app.get('/update.7z', function (req, res) {
-	var fp = filepath+"/update."+vers[vers.length-1]+".7z";
+	var fp = filepath + "/update." + vers[vers.length - 1] + ".7z";
 	var stat = fs.statSync(fp);
 	var file = fs.readFileSync(fp, 'binary');
 	res.setHeader('Content-Length', stat.size);
