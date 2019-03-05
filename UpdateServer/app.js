75587da228c0ed9e196ffb776c52f6faf225aecd
DavidWei <david.wei@schleuniger.com>
2019-03-05 09:02:25 +0800

    pre v0171; update server app

--- UpdateServer/app.js
+++ UpdateServer/app.js
@@ -8,26 +8,35 @@ app.use(bodyParser.urlencoded({
 	extended: false
 }));
 
-var version = 'V01702';
-var filepath = '/home/update/update.01702.7z' // or ../update.zip
+var filepath = '/home/update' // or ../update.zip
 var fileurl = 'http://192.168.16.12:8082/update.7z';
-
+var vers=[];
 if (process.argv[2] == "dev") {
 	filepath = path.normalize(__dirname + "/../release-builds/update.7z") // or ../update.zip
 	fileurl = 'http://127.0.0.1:8082/update.7z';
 }
 const fs = require("fs");
-app.post('/update', function (req, res) {
+app.post('/update',function(req,res) {
+	var dir = fs.readdirSync(filepath)
+	vers = [];
+	for(var i in dir){
+		if(dir[i].indexOf('update.')!=-1){
+			var tv = dir[i].substr(dir[i].indexOf('update.')+7,dir[i].indexOf('.7z')-dir[i].indexOf('update.')-7);
+			if(tv.substr(0,1)=='0') vers.push(tv);
+		}
+	}
 	res.write(JSON.stringify({
-		"last": version,
+		"last": "V"+vers[vers.length-1],
 		"file": fileurl
 	}).replace(/[\/]/g, '\\/'));
 	console.log("reply to post @" + moment().format("HH:mm:ss"))
 	res.end();
-});
+})
+
 app.get('/update.7z', function (req, res) {
-	var stat = fs.statSync(filepath);
-	var file = fs.readFileSync(filepath, 'binary');
+	var fp = filepath+"/update."+vers[vers.length-1]+".7z";
+	var stat = fs.statSync(fp);
+	var file = fs.readFileSync(fp, 'binary');
 	res.setHeader('Content-Length', stat.size);
 	res.setHeader('Content-Type', 'application/zip');
 	res.setHeader('Content-Disposition', 'attachment; filename=update.7z');
