dfd23c7c81286c4f5bf03f272ee6acd57ded7d2f
Wei Liang (David) <David.Wei@schleuniger.com>
2019-04-16 09:58:24 +0800

    v01815

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const request = require('request');
 
 let win;
 let flash;
-global.version = "V01814";
+global.version = "V01815";
 global.appPath = app.getAppPath();
 global.argv = process.argv;
 global.flashClosed = false;
