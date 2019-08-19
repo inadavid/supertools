702dfd75252c17ed18de275c21bdfe905ab9c0a6
Wei Liang (David) <David.Wei@schleuniger.com>
2019-08-19 09:18:39 +0800

    v01906

--- js/sptools.js
+++ js/sptools.js
@@ -6,8 +6,13 @@ $(function(){
         sqltxt = "update [SD30602_STJ201907].[dbo].[l_goodsunit] set isbase=1 where unittype=0 and isbase=0;";
         executeMsSql(sqltxt, function (err, result) {
             if (err) throw err;
-            output.html("rowsAffected: "+result.rowsAffected[0]);
-            btn.prop("disabled",false);
+            var num = result.rowsAffected[0];
+            sqltxt = "update [SD30602_STJ].[dbo].[l_goodsunit] set isbase=1 where unittype=0 and isbase=0;";
+            executeMsSql(sqltxt, function (err, result) {
+                if (err) throw err;
+                output.html("rowsAffected: "+ (num + result.rowsAffected[0]));
+                btn.prop("disabled",false);
+            })
         })
     })
 })
